b9420dab4aaaf001dd191054b334fcc9dae68816
Nayana333 <nrnayana1@gmail.com>
2024-01-13 12:41:41 +0530

    dsa upto sort

--- SLL&DLL ,recursion,array,string/insertionSort.js
+++ SLL&DLL ,recursion,array,string/insertionSort.js
@@ -1,15 +1,12 @@
 function insertionSort(arr){
     for(let i=1;i<arr.length;i++){
         let numberToInsert=arr[i]
-        j=i-1
-        while(j >=0 && arr[j]> numberToInsert){
+
+        let j=i-1;
+        while(j >=0 && arr[j]>numberToInsert){
             arr[j+1]=arr[j]
-            j=j-1
         }
         arr[j+1]=numberToInsert
     }
-    return arr
 }
-let arr=[-2,4,7,1]
-console.log(arr);
-console.log(insertionSort(arr));
+console.log(insertionSort(4,9,1,5));
